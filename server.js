278059ddbfe4858fed859dcece9fd83c4771032b
Sanjar Saydaliyev <92359425+TattiTitto@users.noreply.github.com>
2022-03-30 06:45:26 +0500

    Update server.js

--- server.js
+++ server.js
@@ -35,7 +35,10 @@ app.post("/teacher", urlencodedParser, (req, res) => {
         client.query(`DELETE FROM con_group_student WHERE group_id in(SELECT id FROM groups WHERE teacher_id=${req.body.id})`, (err0, res0) => {console.log(err0);})
         client.query(`DELETE FROM groups WHERE teacher_id=${req.body.id}`, (err0, res0) => {})
         client.query(`DELETE FROM teachers WHERE id=${req.body.id}`, (err0, res0) => {})
-    } else if (req.body.remove == 'groups') client.query(`DELETE FROM groups WHERE teacher_id=${req.body.id}`, (err0, res0) => {})
+    } else if (req.body.remove == 'groups') {
+        client.query(`DELETE FROM con_group_student WHERE group_id=${req.body.id}`, (err0, res0) => {})
+        client.query(`DELETE FROM groups WHERE id=${req.body.id}`, (err0, res0) => {})
+    }
     else client.query(`DELETE FROM con_teacher_subject WHERE teacher_id=${req.body.conid}`, (err0, res0) => {})
     teacher(req, res)
 })
